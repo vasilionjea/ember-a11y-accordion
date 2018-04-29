9c1e6d209f8e7233760631424265e2e4c728f9e3
Billy Onjea <vasili.onjea@gmail.com>
2018-04-28 20:58:07 -0700

    Removes browser styles from the header trigger

--- addon/utils/constants.js
+++ addon/utils/constants.js
@@ -1,6 +1,7 @@
 const CLASS_NAMES = {
   list: 'a11y-accordion-list',
   header: 'a11y-accordion-header',
+  trigger: 'a11y-accordion-header__trigger',
   panel: 'a11y-accordion-panel',
   isExpanded: 'a11y-accordion-is-expanded',
 };
