2f89d09280cfd7f9f398e6275be5c7dd51db43ed
Billy Onjea <vasili.onjea@gmail.com>
2018-09-01 20:38:49 -0700

    Adds gh-pages

---
+++ config/deploy.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(deployTarget) {
+  let ENV = {
+    build: {},
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV.git = {
+      repo: 'git@github.com:vasilionjea/ember-a11y-accordion.git',
+      branch: 'gh-pages',
+      commitMessage: 'Deployed %@'
+    };
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
